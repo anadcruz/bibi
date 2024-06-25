f8bfe813f5049d62c339667d18e5e7c6eea02ee4
Mariana Correa <marianacorrea0304@gmail.com>
2024-06-25 13:28:27 -0300

    Cadastro dos livros

---
+++ src/main/java/Livro.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mariana
+ */
+public class Livro implements Serializable{
+    
+    private static final long serialVersionUID =1L;
+    private Integer idLivro;
+    private String titulo;
+    private String autor;
+    private String editota;
+    private Integer numPag;
+    
+    public Livro () {
+    }
+    
+    public Livro (Integer idLivro) {
+        this.idLivro = idLivro;
+    }
+    
+    public Integer get.IdLivro() {
+        return idLivro;
+    }
+    
+    public void set.IdLivro (Integer idLivro) {
+        this.idLivro = idLivro;
+    }
+}
