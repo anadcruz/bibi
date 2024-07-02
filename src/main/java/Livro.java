1ae24cf78c9446203b9dfcbaf81d2d1586ba392c
Mariana Correa <marianacorrea0304@gmail.com>
2024-07-02 10:10:09 -0300

    Cadastro Livro e Controle do Cadastro

--- src/main/java/Livro.java
+++ src/main/java/Livro.java
@@ -3,19 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ;
 
 /**
  *
  * @author mariana
  */
-public class Livro implements Serializable{
+public class Livro {
     
     private static final long serialVersionUID =1L;
     private Integer idLivro;
     private String titulo;
     private String autor;
-    private String editota;
+    private String editora;
+    private String sinopse;
+    private String idioma;
+    private Integer dataPublic;
     private Integer numPag;
+    private Integer quantExemplares;
+    private String pdfDispo;
+    
     
     public Livro () {
     }
@@ -26,9 +33,87 @@ public class Livro implements Serializable{
     
     public Integer get.IdLivro() {
         return idLivro;
-    }
-    
+    } 
     public void set.IdLivro (Integer idLivro) {
         this.idLivro = idLivro;
     }
+    
+    
+    public String get.Titulo() {
+        return titulo;
+    }
+    public void set.Titulo (string titulo) {
+        this.titulo = titulo;
+    }
+    
+    
+    public String get.Autor() {
+        return autor;
+    }
+    public void set.Autor (string autor) {
+        this.autor = autor;
+    }
+
+    
+    public String get.Editora() {
+        return editora;
+    }
+    public void set.Editora (string editora) {
+        this.editora = editora;
+    }
+    
+    
+    public String get.Sinopse() {
+        return sinopse;
+    }
+    public void set.Sinopse (string sinopse) {
+        this.sinopse = sinopse;
+    }
+    
+    
+    public String get.Idioma() {
+        return idioma;
+    }
+    public void set.Idioma (string idioma) {
+        this.idioma = idioma;
+    }
+    
+    
+    public Integer get.DataPublic() {
+        return dataPublic;
+    }
+    public void set.DataPublic (Integer dataPublic) {
+        this.dataPublic = dataPublic;
+    }
+
+    
+    public Integer get.NumPag() {
+        return numPag;
+    }
+    public void set.NumPag (Integer numPag) {
+        this.numPag = numPag;
+    }
+    
+    
+    public Integer get.QuantExemplares() {
+        return quantExemplares;
+    }
+    public void set.QuantExemplares (Integer quantExemplares) {
+        this.quantExemplares = quantExemplares;
+    }
+    
+    
+    public String get.PdfDispo() {
+        return pdfDispo;
+    }
+    public void set.PdfDispo (string pdfDispo) {
+        this.pdfDispo = pdfDispo;
+    }
+
+    public static void main(String[] args){
+    
+    }
+    
 }
+
+
